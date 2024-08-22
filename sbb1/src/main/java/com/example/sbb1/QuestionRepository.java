08758105f30df85c1097b81f39d65a2adfdb0314
kdhhyunjin1 <kdhhyunjin1@gmail.com>
2024-08-22 17:57:54 +0900

    Day3
    Create Table
    Insert, Update, Delete Record
    on h2-console

---
+++ sbb1/src/main/java/com/example/sbb1/QuestionRepository.java
@@ -0,0 +1,10 @@
+package com.example.sbb1;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface QuestionRepository extends JpaRepository<Question,Integer> {
+    Question findBySubject(String subject);
+    Question findBySubjectAndContent(String subject,String Content);
+    List<Question> findBySubjectLike(String subject);
+}
