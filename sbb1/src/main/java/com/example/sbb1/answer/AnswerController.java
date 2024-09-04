48d5c81335177476314ba0148fe62961ea90f501
kdhhyunjin1 <kdhhyunjin1@gmail.com>
2024-09-04 19:21:59 +0900

    day7
    Active Submit Button

--- sbb1/src/main/java/com/example/sbb1/answer/AnswerController.java
+++ sbb1/src/main/java/com/example/sbb1/answer/AnswerController.java
@@ -3,9 +3,11 @@ package com.example.sbb1.answer;
 
 import com.example.sbb1.question.Question;
 import com.example.sbb1.question.QuestionService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,9 +21,13 @@ public class AnswerController {
     private final AnswerService answerService;
 
     @PostMapping("/create/{id}") //POST 방식으로 매핑. GET방식은 URL(header)에 변수를 넣어 호출하기때문에 보안이 취약하지만 POST방식은 body에 변수를 넣어 호출하기 때문에 기본적인 보안이 설정되어있음.
-    public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam(value = "content") String content) {
+    public String createAnswer(Model model, @PathVariable("id") Integer id, @Valid AnswerForm answerForm, BindingResult bindingResult) {
         Question question = this.questionService.getQuestion(id);
-        this.answerService.create(question, content);
+        if(bindingResult.hasErrors()){
+            model.addAttribute("question",question);
+            return "question_detail";
+        }
+        this.answerService.create(question, answerForm.getContent());
         return String.format("redirect:/question/detail/%s", id);
     }
 }
